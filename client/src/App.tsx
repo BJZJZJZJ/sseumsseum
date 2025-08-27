600d21289707a66bedf47443ab1a210b15014ef3
devdeok <deokin.kim@cocoa-edu.com>
2025-08-27 16:56:09 +0900

    feat: SA-42 #comment 회원가입 페이지 제작

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage'; // 1. 로그인 페이지 import
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage'; // 1. 회원가입 페이지 import
 import MainLayout from './components/layout/MainLayout';
 
 function App() {
@@ -13,9 +14,7 @@ function App() {
 
       {/* 레이아웃이 없는 독립적인 페이지들 */}
       <Route path="/login" element={<LoginPage />} /> 
-      
-      {/* TODO: 여기에 가입 페이지 등 다른 독립 페이지 라우트를 추가하세요 */}
-      {/* 예: <Route path="/signup" element={<SignupPage />} /> */}
+      <Route path="/signup" element={<SignupPage />} /> {/* 2. 라우트 추가 */}
     </Routes>
   );
 }
