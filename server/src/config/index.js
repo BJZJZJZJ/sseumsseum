103b545c6e01aa3ec1e6a4bea662147ff6792013
BJZJZJZJ <66ap66ap@gmail.com>
2025-08-27 20:17:09 +0900

    feat:SA-38 #comment API 문서 보안을 위한 로그인 추가

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -25,6 +25,9 @@ const SMTP_PASS = process.env.SMTP_PASS;
 const EMAIL_FROM = process.env.EMAIL_FROM;
 const TOKEN_EXPIRES_MIN = process.env.TOKEN_EXPIRES_MIN;
 
+const AUTH_USER = process.env.AUTH_USER;
+const AUTH_PASSWORD = process.env.AUTH_PASSWORD;
+
 module.exports = {
   CORS_ALLOWED_ORIGIN,
   SERVER_URL,
@@ -34,6 +37,9 @@ module.exports = {
   JWT_ACCESS_EXPIRY,
   JWT_REFRESH_TOKEN_SECRET,
   JWT_REFRESH_EXPIRY,
+
+  AUTH_USER,
+  AUTH_PASSWORD,
 
   SMTP_HOST,
   SMTP_PORT,
--- server/src/middleware/rateLimit.js
+++ server/src/middleware/rateLimit.js
@@ -4,7 +4,6 @@ const RESEND_LIMIT = rateLimit({
   windowMs: 60 * 1000, // 1분
   max: 3,
   message: {
-    success: false,
     message: "요청이 너무 많습니다. 잠시 후 다시 시도해 주세요.",
   },
 });
